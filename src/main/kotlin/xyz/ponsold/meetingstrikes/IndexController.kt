8938afbdc4f5c3f0e04de74a4f97d4da052afe15
Philipp Ponsold <philipp.ponsold@gmail.com>
2020-12-02 21:27:20 +0100

    Adds IndexController

---
+++ src/main/kotlin/xyz/ponsold/meetingstrikes/IndexController.kt
@@ -0,0 +1,16 @@
+package xyz.ponsold.meetingstrikes
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+class IndexController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    fun home(): String {
+        return "Hello from meeting-strikes"
+    }
+
+}
