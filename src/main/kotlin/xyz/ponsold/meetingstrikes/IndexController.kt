51cb42b98742df5ed760cb6613fdfae7cbc151e6
Philipp Ponsold <philipp.ponsold@gmail.com>
2020-12-02 21:43:30 +0100

    Adds test for Github Actions deployment

--- src/main/kotlin/xyz/ponsold/meetingstrikes/IndexController.kt
+++ src/main/kotlin/xyz/ponsold/meetingstrikes/IndexController.kt
@@ -10,7 +10,7 @@ class IndexController {
     @RequestMapping("/")
     @ResponseBody
     fun home(): String {
-        return "Hello from meeting-strikes"
+        return "Hello after Github Push meeting-strikes"
     }
 
 }
